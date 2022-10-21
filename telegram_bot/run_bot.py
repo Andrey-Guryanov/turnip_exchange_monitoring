76e8164f72f2c5382a0d6f0a153ae5d4f54429bb
Andrey-Guryanov <asdfGG564321>
2022-10-21 11:06:55 +0300

    fix: docker-compose.yml, run_bot.py

--- telegram_bot/run_bot.py
+++ telegram_bot/run_bot.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 from dotenv import load_dotenv
 from aiogram import Router, Dispatcher
@@ -7,7 +8,7 @@ from globals import bot
 from components import (
     router_task_no_cost,
     router_task_cost,
-    router_control_users,)
+    router_control_users, )
 from components.sender import send_messages_task
 
 try:
@@ -15,7 +16,6 @@ try:
     from common_modules.model_mongo import Mongo_db
 except ModuleNotFoundError:
     import sys
-
     sys.path.append(
         os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
     from common_modules.app_settings import load_settings
@@ -26,9 +26,6 @@ load_dotenv()
 dp = Dispatcher()
 router = Router()
 scheduler = AsyncIOScheduler()
-
-
-
 
 
 async def main() -> None:
